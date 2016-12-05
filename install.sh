f39d2aee60c51b7f018d891002937ad4edb02f40
Tanner Rollefson <trollefson@gmail.com>
2016-12-04 18:10:08 -0600

    Sym link ctags file

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
 ln -sfv "$DOTFILES_DIR/tmux/tmux.conf" ~
+ln -sfv "$DOTFILES_DIR/ctags/.ctags" ~
 
 # Package managers & packages
 
