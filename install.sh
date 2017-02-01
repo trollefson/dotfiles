8a8c041977983f59a2828a0ae108168f629ef1db
Tanner Rollefson <trollefson@gmail.com>
2017-02-01 06:16:56 -0600

    Add zsh support

--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ EXTRA_DIR="$HOME/.extra"
 
 # Bunch of symlinks
 
+ln -sfv "$DOTFILES_DIR/runcom/.zshrc" ~
+ln -sfv "$DOTFILES_DIR/runcom/.oh-my-zsh" ~
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
