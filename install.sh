7542d125a8127c31b2bc31a686fca91ee47bc4b3
trollefson <trollefson@gmail.com>
2023-02-20 20:10:48 -0600

    remove brew install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-# Bunch of symlinks
+# Create symlinks to home dir
 ln -sfv /Users/tanner/Personal/Projects/dotfiles/inputrc ~
 ln -sfv /Users/tanner/Personal/Projects/dotfiles/git/.gitconfig ~
 ln -sfv /Users/tanner/Personal/Projects/dotfiles/git/.gitignore_global ~
@@ -9,9 +9,6 @@ ln -sfv /Users/tanner/Personal/Projects/dotfiles/tmux/.tmux-powerline ~
 ln -sfv /Users/tanner/Personal/Projects/dotfiles/ctags/.ctags ~
 ln -sfv /Users/tanner/Personal/Projects/dotfiles/.zshrc ~
 ln -sfv /Users/tanner/Personal/Projects/dotfiles/alias ~
-
-# Run brew installs
-# zsh install/brew.sh
 
 # Install Vim Plugins from vim/.vimrc
 vim +PluginInstall +qall
